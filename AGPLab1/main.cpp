300b997c333dc72a8a463c383163cedcf35aad0a
shockk <shokku.ra@gmail.com>
2013-09-27 12:21:45 +0100

    Initial Commit

---
+++ AGPLab1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  AGPLab1
+//
+//  Created by Shockk on 27/09/2013.
+//  Copyright (c) 2013 Shockk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
