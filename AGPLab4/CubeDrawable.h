b174c095742cbf0c80a8ffab6b7e2ee2d91cbe45
Shockk <shokku.ra@gmail.com>
2013-10-31 16:59:41 +0000

    Fixed error in bitangent calculations.

--- AGPLab4/CubeDrawable.h
+++ AGPLab4/CubeDrawable.h
@@ -137,7 +137,7 @@ protected:
             // calculate tangent and bitangent (what is this magic?!)
             float r = 1.0f / (deltaUV1.x * deltaUV2.y - deltaUV1.y * deltaUV2.x);
             glm::vec3 tangent   = (deltaPos1 * deltaUV2.y - deltaPos2 * deltaUV1.y) * r;
-            glm::vec3 bitangent = (deltaPos2 * deltaUV1.x - deltaPos1 * deltaUV1.x) * r;
+            glm::vec3 bitangent = (deltaPos2 * deltaUV1.x - deltaPos1 * deltaUV2.x) * r;
 
             // put everything into the buffers
             for(int j=0; j<4; ++j)
