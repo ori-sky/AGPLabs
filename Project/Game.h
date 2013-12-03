e4f5c60570e9d04424872fa247733a3876c3133c
Shockk <shokku.ra@gmail.com>
2013-12-03 20:02:02 +0000

    Changed number of particles to 1,000.

--- Project/Game.h
+++ Project/Game.h
@@ -59,7 +59,7 @@ protected:
     CubeDrawable cube_right;
     ParticlesDrawable particles;
 public:
-    Game() : particles(10000) {}
+    Game() : particles(1000) {}
     static Game * New(void) { return new Game(); }
     void PrintShaderError(GLint shader);
 
